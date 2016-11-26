f03aad811cc19e268c83e4640bb9980b1a356e2a
pritam hinger <pritamgenius24@gmail.com>
2016-11-27 01:27:34 +0530

    Added code for scheduling local notification.

--- WakeMeUpPOC/AppDelegate.swift
+++ WakeMeUpPOC/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    let notificationDelegate = ViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        center.delegate = notificationDelegate
         return true
     }
 
